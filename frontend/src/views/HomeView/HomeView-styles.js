a275ffc3418d4f328f589c21cfecbe895c8dd5f3
root <andersbjorklund@live.com>
2020-07-08 10:42:02 +0000

    refactored styles

--- frontend/src/views/HomeView/HomeView-styles.js
+++ frontend/src/views/HomeView/HomeView-styles.js
@@ -1,29 +1,21 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const Styles = styled.div`
-.wrapper {
-  display: grid;
-  grid-template-columns: 50% 50%;
-}
-
-.hero-text {
+export const HeroText = styled.p`
   margin: 40px 0;
   width: 80%;
-}
+`;
 
-.cta-button {
+export const HeroImage = styled.img`
+  margin-top: 30px;
+  width: 100%;
+`;
+
+export const CTAButton = styled(Link)`
   border: none;
   border-radius: 125px;
   padding: 15px 30px;
   background-color: #E24F54;
   background-color: #f6cd42;
   color: #111;
-}
-
-img {
-  margin-top: 30px;
-  width: 100%;
-}
 `;
-
-export default Styles;
--- frontend/src/views/HomeView/HomeView.jsx
+++ frontend/src/views/HomeView/HomeView.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Row, Col } from 'react-bootstrap';
 import BaseLayout from '../../layouts/BaseLayout';
-import Styles from './HomeView-styles';
+import { HeroText, HeroImage, CTAButton } from './HomeView-styles';
 
 const HomeView = () => (
   <BaseLayout>
-    <Styles>
-      <Row>
-        <Col lg>
-          <h1>Simple job tracking</h1>
-          <h3>Dont waste time keeping track of job applications</h3>
-          <p className="hero-text">
-            When searching for a job, sending out an application is quick.
-            Being organized and knowing where you are in the process with
-            each of them can be a challenge.
-            Let Applyr help you with this problem.
-          </p>
-          <Link className="cta-button" to="/register">Get started!</Link>
-        </Col>
-        <Col>
-          <img alt="#" src="/img/hero-image.jpg" />
-        </Col>
-      </Row>
-    </Styles>
+    <Row>
+      <Col lg>
+        <h1>Simple job tracking</h1>
+        <h3>Dont waste time keeping track of job applications</h3>
+        <HeroText className="hero-text">
+          When searching for a job, sending out an application is quick.
+          Being organized and knowing where you are in the process with
+          each of them can be a challenge.
+          Let Applyr help you with this problem.
+          </HeroText>
+        <CTAButton className="cta-button" to="/register">Get started!</CTAButton>
+      </Col>
+      <Col>
+        <HeroImage alt="#" src="/img/hero-image.jpg" />
+      </Col>
+    </Row>
   </BaseLayout>
 );
 
