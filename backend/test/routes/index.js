89493feb0090544645a30815314406f77d61a242
Anders Jensen <andersbjorklund@live.com>
2020-08-17 15:21:27 +0200

    Update gitignore

--- backend/test/routes/index.js
+++
@@ -1,6 +0,0 @@
-/* globals describe */
-
-describe('Routes', () => {
-  describe('/api/user/* - User routes', require('./user'));
-  describe('/api/job/* - Job routes', require('./job'));
-});
--- backend/test/routes/user/index.js
+++
@@ -1,8 +0,0 @@
-/* globals describe */
-
-module.exports = function () {
-  describe('[POST] /api/user/username - Check if username is taken', require('./username'));
-  describe('[POST] /api/user/register - Register user', require('./register'));
-  describe('[POST] /api/user/register - Login user', require('./login'));
-  describe('[POST] /api/user/logout - Logout user', require('./logout'));
-};
