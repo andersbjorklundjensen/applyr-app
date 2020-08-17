62a89046d4b23379b6d185a4151211cf6c25b10f
Anders Jensen <andersbjorklund@live.com>
2020-08-17 15:41:17 +0200

    Move tests for POST /api/user/logout

---
+++ backend/src/routes/user/logout.test.js
@@ -0,0 +1,27 @@
+/* globals before, after, it */
+
+const supertest = require('supertest');
+const app = require('../..')();
+const userTestUtils = require('./utils/test');
+
+const server = app.listen();
+const route = '/api/user/logout';
+
+describe('POST /api/user/logout', () => {
+  beforeEach(async () => {
+    await app.locals.db.dropDatabase();
+  });
+
+  it('should logout a user when a user is correctly logged in', async () => {
+    const user = await userTestUtils.createUserInDb(server);
+
+    return supertest(server)
+      .post(route)
+      .set('authorization', user.token)
+      .expect(200);
+  });
+
+  it('should not logout a user without authorization header', async () => supertest(server)
+    .post(route)
+    .expect(401));
+});
