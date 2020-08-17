b3f89c751cc0f5dba207e5341ff19ede42cd9b7a
Anders Jensen <andersbjorklund@live.com>
2020-08-17 15:58:23 +0200

    Add test utils functions for user api

---
+++ backend/src/routes/user/utils/test/index.js
@@ -0,0 +1,27 @@
+const crypto = require('crypto');
+const supertest = require('supertest');
+
+const constructUser = () => {
+  return {
+    username: crypto.randomBytes(6).toString('hex'),
+    password: crypto.randomBytes(6).toString('hex'),
+    token: null,
+  };
+}
+
+const createUserInDb = async (server) => {
+  const user = constructUser();
+
+  await supertest(server)
+    .post('/api/user/register')
+    .send(user)
+    .expect(200)
+    .then((response) => {
+      user.token = response.body.token;
+    });
+
+  return user;
+}
+
+exports.constructUser = constructUser;
+exports.createUserInDb = createUserInDb;
