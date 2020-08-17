96162d1c54eef36341b12f073ed6e584305f651b
Anders Jensen <andersbjorklund@live.com>
2020-08-17 15:54:08 +0200

    Move tests for POST /api/user/register

---
+++ backend/src/routes/user/register.test.js
@@ -0,0 +1,57 @@
+/* globals before, after, it */
+
+const supertest = require('supertest');
+const app = require('../..')();
+const userTestUtils = require('./utils/test');
+
+const server = app.listen();
+
+const route = '/api/user/register';
+
+describe('POST /api/user/register', () => {
+  beforeEach(async () => {
+    await app.locals.db.dropDatabase();
+  });
+
+  it('should register a user with all information correctly supplied', async () => {
+    const user = userTestUtils.constructUser();
+
+    return supertest(server)
+      .post(route)
+      .send(user)
+      .expect(200);
+  });
+
+  it('should not register a user with invalid username', async () => {
+    const user = userTestUtils.constructUser();
+
+    return supertest(server)
+      .post(route)
+      .send({
+        ...user,
+        username: null,
+      })
+      .expect(400);
+  });
+
+  it('should not register a user with invalid password', async () => {
+    const user = userTestUtils.constructUser();
+
+    return supertest(server)
+      .post(route)
+      .send({
+        ...user,
+        password: null,
+      })
+      .expect(400);
+  });
+
+  it('should not register a user which already exists', async () => {
+    const user = await userTestUtils.createUserInDb(server);
+
+    return supertest(server)
+      .post(route)
+      .send(user)
+      .expect(400);
+  });
+});
