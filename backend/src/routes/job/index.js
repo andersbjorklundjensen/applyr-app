182f5733488035834973c8b6c8e5708f3c38cb99
Anders Jensen <andersbjorklund@live.com>
2020-08-18 15:34:31 +0200

    Add upload middleware to PUT /api/job/:id route

--- backend/src/routes/job/index.js
+++ backend/src/routes/job/index.js
@@ -36,7 +36,7 @@ const upload = multer({
 router.get('/api/job/all', authCheck, require('./get-all-jobs'));
 router.get('/api/job/:id', authCheck, require('./get-job'));
 router.post('/api/job', authCheck, upload, require('./add-job'));
-router.put('/api/job/:id', authCheck, require('./edit-job'));
+router.put('/api/job/:id', authCheck, upload, require('./edit-job'));
 router.delete('/api/job/:id', authCheck, require('./delete-job'));
 
 module.exports = router;
