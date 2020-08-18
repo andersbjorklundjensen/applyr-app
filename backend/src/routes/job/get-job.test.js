85501980e9fdc3ded616c7bdc2a9404ccc89ecc4
Anders Jensen <andersbjorklund@live.com>
2020-08-18 15:34:00 +0200

    Change tests to use new test util functions

--- backend/src/routes/job/get-job.test.js
+++ backend/src/routes/job/get-job.test.js
@@ -1,48 +1,46 @@
 /* globals before, after, it */
 
 const supertest = require('supertest');
-const Auth = require('../../utils/User');
-const Job = require('../../utils/Job');
-const app = require('../../../src')();
+const app = require('../..')();
+const userTestUtils = require('../user/utils/test');
+const jobTestUtils = require('./utils/test');
 
 const server = app.listen();
 const route = '/api/job/';
 
-module.exports = function () {
-  const auth = new Auth(app, server);
-  const job = new Job(app, server);
-
-  before(async () => {
+describe('GET /api/job/:id', () => {
+  beforeEach(async () => {
     await app.locals.db.dropDatabase();
   });
 
-  after(async () => {
-    app.locals.db.close();
-    server.close();
-  });
+  afterAll((done) => {
+    server.close(done);
+  })
 
   it('should get a job for a user correctly', async () => {
-    const user = await auth.createUser();
-    const newJob = await job.createJob(user);
+    const user = await userTestUtils.createUserInDb(server);
+    const newJob = await jobTestUtils.createJobInDb(server, user);
 
     return supertest(server)
       .get(route + newJob.id)
-      .set('Authorization', user.token)
+      .set('authorization', user.token)
       .expect(200);
   });
 
   it('should not get a job for a user if the job does not belong to the user', async () => {
-    const user = await auth.createUser();
-    const user2 = await auth.createUser();
-    const newJob = await job.createJob(user);
+    const user = await userTestUtils.createUserInDb(server);
+    const user2 = await userTestUtils.createUserInDb(server);
+    const newJob = await jobTestUtils.createJobInDb(server, user);
 
     return supertest(server)
       .get(route + newJob.id)
-      .set('Authorization', user2.token)
-      .expect(401);
+      .set('authorization', user2.token)
+      .expect(400);
   });
 
-  it('should not get a job for a user without authorization header', async () => supertest(server)
-    .get(`${route}asdf`)
-    .expect(401));
-};
+  it('should not get a job for a user without authorization header', async () => {
+    return supertest(server)
+      .get(`${route}asdf`)
+      .expect(401);
+  });
+});
