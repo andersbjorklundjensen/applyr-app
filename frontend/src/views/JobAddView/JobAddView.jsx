a3ebd7e90f6ce8298deea6563efb2e7e080962f8
Anders Jensen <andersbjorklund@live.com>
2020-08-20 07:56:12 +0000

    Refactor field components to for useForm hook

--- frontend/src/views/JobAddView/JobAddView.jsx
+++ frontend/src/views/JobAddView/JobAddView.jsx
@@ -34,12 +34,22 @@ const JobAddView = () => {
   return (
     <BaseLayout>
       <Styles>
-        <form className="job-form" onSubmit={(e) => onFormSubmit(e)}>
-          <Field label="Position title:" type="text" value={positionTitle} onChange={(e) => setPositionTitle(e.target.value)} maxLength="50" />
-          <Field label="Location:" type="text" value={location} onChange={(e) => setLocation(e.target.value)} maxLength="50" />
-          <Field label="Company:" type="text" value={company} onChange={(e) => setCompany(e.target.value)} maxLength="50" />
-          <Field label="Link to job ad:" type="url" value={link} onChange={(e) => setLink(e.target.value)} maxLength="250" />
-          <Field label="Application date:" type="date" value={dateApplied} onChange={(e) => setDateApplied(e.target.value)} />
+        <form className="job-form" onSubmit={handleSubmit(onFormSubmit)}>
+          <Field register={register({ required: "Missing position title" })}
+            error={errors.positionTitle} name="positionTitle"
+            label="Position title:" type="text" maxLength="50" />
+          <Field register={register({ required: "Missing location" })}
+            error={errors.location} name="location"
+            label="Location:" type="text" maxLength="50" />
+          <Field register={register({ required: "Missing company name" })}
+            error={errors.company} name="company"
+            label="Company:" type="text" maxLength="50" />
+          <Field register={register({ required: "Missing link" })}
+            error={errors.linkToPosting} name="linkToPosting"
+            label="Link to job ad:" type="url" maxLength="250" />
+          <Field register={register({ required: "Missing application date" })}
+            error={errors.dateApplied} name="dateApplied"
+            label="Application date:" type="date" />
           <div>
             Current status:
             <select className="selector" ref={register} name="currentStatus">
@@ -48,16 +58,18 @@ const JobAddView = () => {
               ))}
             </select>
           </div>
-          <FileInput id="cv" label="CV: " onChange={(e) => setCv(e.target.files[0])} />
-          <FileInput id="coverLetter" label="Cover letter: " onChange={(e) => setCoverLetter(e.target.files[0])} />
+          <FileInput register={register} name="cv"
+            id="cv" label="CV: " />
+          <FileInput register={register} name="coverLetter"
+            id="coverLetter" label="Cover letter: " />
           <div>
-            Notes: <textarea value={notes} onChange={(e) => setNotes(e.target.value)} maxLength="5000" />
+            Notes: <textarea ref={register} name="notes" maxLength="5000" />
           </div>
           <div></div>
           <Button color="green" fillBlock type="submit">Add job</Button>
         </form>
       </Styles>
-    </BaseLayout >
+    </BaseLayout>
   )
 }
 
