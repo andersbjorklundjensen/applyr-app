5aa7c61684f13fc06f90ef3d402915b4b8750278
root <andersbjorklund@live.com>
2020-11-24 22:14:12 +0000

    Add api wrapper function for getting metadata for all files for a specific job

---
+++ frontend/src/api/files/getAllFilesByJobId.js
@@ -0,0 +1,21 @@
+import api from '../../config/api';
+
+const getAllFilesByJobId = async (jobId, token) => {
+  const request = await fetch(`${api.API_URL}/files/${jobId}`, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': token,
+    },
+  });
+
+  const response = await request.json();
+
+  return {
+    data: response,
+    error: response.message
+  }
+}
+
+export default getAllFilesByJobId;
