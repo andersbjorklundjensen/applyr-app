f8a4e5f50f99c32c9b505aed01d6b950fdfbd352
root <andersbjorklund@live.com>
2020-11-24 22:13:29 +0000

    Add api wrapper function for deleting a file

---
+++ frontend/src/api/files/deleteFileById.js
@@ -0,0 +1,21 @@
+import api from '../../config/api';
+
+const deleteFileById = async (fileId, token) => {
+  const request = await fetch(`${api.API_URL}/file/${fileId}`, {
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': token,
+    },
+  });
+
+  const response = await request.json();
+
+  return {
+    data: response,
+    error: response.message
+  }
+}
+
+export default deleteFileById;
