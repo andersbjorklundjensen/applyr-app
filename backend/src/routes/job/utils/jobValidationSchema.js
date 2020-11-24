013047997ab3e7cd1fdbd373ec0ba31583f44435
root <andersbjorklund@live.com>
2020-11-24 13:20:26 +0000

    Add validation schema for job data type

---
+++ backend/src/routes/job/utils/jobValidationSchema.js
@@ -0,0 +1,51 @@
+const Joi = require('joi');
+
+const jobValidationSchema = Joi.object({
+  positionTitle: Joi
+    .string()
+    .alphanum()
+    .min(3)
+    .max(50)
+    .required(),
+
+  location: Joi
+    .string()
+    .required()
+    .max(50),
+
+  linkToPosting: Joi
+    .string()
+    .uri()
+    .required()
+    .max(250),
+
+  company: Joi
+    .string()
+    .required()
+    .max(50),
+
+  dateApplied: Joi
+    .number()
+    .required(),
+
+  currentStatus: Joi
+    .number()
+    .required()
+    .allow(1, 2, 3, 4, 5),
+
+  notes: Joi
+    .string()
+    .allow("")
+    .max(5000)
+})
+
+/**
+ * Valid current statuses that a job can be in:
+ * - 1 = applied
+ * - 2 = interviewing
+ * - 3 = under review
+ * - 4 = offer received
+ * - 5 = rejected
+ */
+
+module.exports = jobValidationSchema;
