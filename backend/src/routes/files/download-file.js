b36603ecc270554509f6ab41834248142215a43d
root <andersbjorklund@live.com>
2020-11-24 22:09:05 +0000

    Add api function for downloading files

---
+++ backend/src/routes/files/download-file.js
@@ -0,0 +1,12 @@
+
+module.exports = async (req, res) => {
+  const { id: fileId } = req.params;
+
+  const file = await req.app.locals.db.models.files.findOne({ _id: fileId }).lean();
+  if (!file) return res.status(400).send('file does not exist');
+
+  const job = await req.app.locals.db.models.jobs.findOne({ _id: file.jobId, ownerId: res.locals.userId }).lean();
+  if (!job) return res.status(400).send('file is inaccessible');
+
+  res.download(`uploads/${file.path}`);
+};
