d4b57fd66a5d37d9a86030a11b0d0b6f62d6ba79
root <andersbjorklund@live.com>
2020-11-24 22:14:30 +0000

    Add api wrapper function for uploading a file

---
+++ frontend/src/api/files/uploadFile.js
@@ -0,0 +1,23 @@
+import api from '../../config/api';
+
+const uploadFile = async (file, jobId, token) => {
+  let formData = new FormData();
+  formData.append('file', file)
+
+  const request = await fetch(`${api.API_URL}/file/${jobId}`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': token,
+    },
+    body: formData
+  });
+
+  const response = await request.json();
+
+  return {
+    data: response
+  };
+}
+
+export default uploadFile;
