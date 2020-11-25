1d39700dca450f6469dc894ae87c3776852cdb20
root <andersbjorklund@live.com>
2020-11-25 16:16:50 +0000

    Refactor home view styles

--- frontend/src/views/HomeView/HomeView.jsx
+++ frontend/src/views/HomeView/HomeView.jsx
@@ -1,26 +1,29 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react'
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import BaseLayout from '../../layouts/BaseLayout';
-import { HeroText, HeroImage, CTAButton } from './HomeView-styles';
+import { Link } from 'react-router-dom';
 
 const HomeView = () => (
   <BaseLayout>
-    <Row>
-      <Col lg>
-        <h1>Simple job tracking</h1>
-        <h3>Dont waste time keeping track of job applications</h3>
-        <HeroText className="hero-text">
+    <div className="container md:flex">
+      <div className="md:w-1/2">
+        <h1 className="text-4xl font-semibold">Simple job tracking</h1>
+        <h3 className="text-2xl font-semibold">Dont waste time keeping track of job applications</h3>
+        <div className="my-4 w-4/5">
           When searching for a job, sending out an application is quick.
           Being organized and knowing where you are in the process with
           each of them can be a challenge.
           Let Applyr help you with this problem.
-          </HeroText>
-        <CTAButton className="cta-button" to="/register">Get started!</CTAButton>
-      </Col>
-      <Col>
-        <HeroImage alt="#" src="/img/hero-image.jpg" />
-      </Col>
-    </Row>
+          </div>
+        <Link css={css`background-color: #f6cd42;`}
+          className="inline-block rounded-full py-3 px-4" to="/register">Get started!</Link>
+      </div>
+      <div className="md:w-1/2">
+        <img className="mt-2 w-full" alt="#" src="/img/hero-image.jpg" />
+      </div>
+    </div>
   </BaseLayout>
 );
 
