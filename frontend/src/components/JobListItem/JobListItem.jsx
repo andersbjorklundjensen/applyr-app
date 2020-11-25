6cdae0b2be75bd5b19d30cfdaeaf7ae1cdbf0492
root <andersbjorklund@live.com>
2020-11-25 17:44:22 +0000

    Refactor styles for jobListItem component

--- frontend/src/components/JobListItem/JobListItem.jsx
+++ frontend/src/components/JobListItem/JobListItem.jsx
@@ -1,39 +1,27 @@
 import React from 'react';
-import Styles from './JobListItem-styles';
 import { Link } from 'react-router-dom';
 import * as moment from 'moment';
 
 const JobListItem = ({ job }) => {
   const jobStatuses = ["", "Applied", "Interviewing", "Under review", "Offer received", "Rejected"];
+
   return (
-    <Styles>
-      <div className="job-list-item">
-        <div className="job-location">
-          <Link to={`/job/${job._id}`}>
-            Position title: {job.positionTitle}
-          </Link>
-        </div>
-        <div className="job-list-item-row">
-          <div>
-            Company: {job.company}
-          </div>
-          <div>
-            Location: {job.location}
-          </div>
-          <div>
-            Application date: {moment(job.dateApplied).format('DD.MM.YYYY')}
-          </div>
-        </div>
-        <div className="job-list-item-row">
-          <div>
-            Current status: {jobStatuses[job.currentStatus]}
-          </div>
-          <div>
-            Link to job ad: {job.linkToPosting}
-          </div>
+    <div className="bg-gray-100 px-4 py-2 my-4 rounded-lg border-2 border-blue-200 overflow-x-scroll">
+      <div className="text-2xl text-blue-500">
+        <Link to={`/job/${job._id}`}>Position title: {job.positionTitle}</Link>
+      </div>
+      <div className="md:flex md:justify-between">
+        <div>Company: {job.company}</div>
+        <div>Location: {job.location}</div>
+        <div>
+          Application date: {moment(job.dateApplied).format('DD.MM.YYYY')}
         </div>
       </div>
-    </Styles>
+      <div className="">
+        <div>Current status: {jobStatuses[job.currentStatus]}</div>
+        <div>Link to job ad: {job.linkToPosting}</div>
+      </div>
+    </div>
   )
 }
 
