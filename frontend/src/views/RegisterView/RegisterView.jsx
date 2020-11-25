ea5e47aaf60d4de2a557a8be21125d8a2d9a8027
root <andersbjorklund@live.com>
2020-11-25 16:41:19 +0000

    Refactor register view styles

--- frontend/src/views/RegisterView/RegisterView.jsx
+++ frontend/src/views/RegisterView/RegisterView.jsx
@@ -1,10 +1,12 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/react'
+
 import React, { useState, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import Loader from 'react-loader-spinner';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import { AuthContext } from '../../contexts/AuthContext';
 import BaseLayout from '../../layouts/BaseLayout';
-import Styles from './RegisterView-styles';
 import Field from '../../components/Field/Field';
 import { useForm } from 'react-hook-form';
 import registerUser from '../../api/user/register';
@@ -41,39 +43,38 @@ const RegisterView = () => {
 
   return (
     <BaseLayout>
-      <Styles>
-        <div className="sign-up">
-          <div className="form-wrapper">
-            <h1>Sign up</h1>
-            <form
-              className="signup-form"
-              onSubmit={handleSubmit(onSignUpFormSubmit)}
-            >
-              <div>
-                <Field register={register({ required: "Missing username" })}
-                  name="username" error={errors.username}
-                  type="text" placeholder="Username" maxLength="50" />
-              </div>
-              <div>
-                <Field
-                  register={register({
-                    required: "Missing password",
-                    minLength: { value: 8, message: "Password is too short" }
-                  })}
-                  name="password" error={errors.password}
-                  type="password" placeholder="Password" minLength="8" maxLength="50" />
-              </div>
-              <input type="submit" value="Sign up" />
-            </form>
-            {isLoading && (
-              <div className="spinner-wrapper">
-                <Loader visible={isLoading} type="TailSpin" color="#00BFFF" height={50} width={50} />
-                <div>Signing up...</div>
-              </div>
-            )}
-          </div>
+      <div className="flex justify-center">
+        <div className="mt-16 text-center">
+          <h1 className="text-4xl font-semibold">Sign up</h1>
+          <form
+            css={css`* { margin: 15px 0; }`}
+            onSubmit={handleSubmit(onSignUpFormSubmit)}
+          >
+            <div>
+              <Field register={register({ required: "Missing username" })}
+                name="username" error={errors.username}
+                type="text" placeholder="Username" maxLength="50" />
+            </div>
+            <div>
+              <Field
+                register={register({
+                  required: "Missing password",
+                  minLength: { value: 8, message: "Password is too short" }
+                })}
+                name="password" error={errors.password}
+                type="password" placeholder="Password" minLength="8" maxLength="50" />
+            </div>
+            <input css={css`background-color: #E24F54;`}
+              className="w-full rounded-full py-2.5 text-white" type="submit" value="Sign up" />
+          </form>
+          {isLoading && (
+            <div className="flex justify-center items-center">
+              <Loader visible={isLoading} type="TailSpin" color="#00BFFF" height={50} width={50} />
+              <div className="mx-3">Signing up...</div>
+            </div>
+          )}
         </div>
-      </Styles>
+      </div>
     </BaseLayout>
   );
 };
