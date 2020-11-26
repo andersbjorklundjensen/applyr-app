61eb12b4a1caaf32a38e7eff9ead1549638c5233
root <andersbjorklund@live.com>
2020-11-26 09:48:41 +0000

    Refactor settings view styles

--- frontend/src/views/SettingsView/SettingsView.jsx
+++ frontend/src/views/SettingsView/SettingsView.jsx
@@ -1,15 +1,10 @@
 import React, { useContext, useState, useEffect } from 'react';
-import {
-  Row,
-  Col,
-} from 'react-bootstrap';
 import BaseLayout from '../../layouts/BaseLayout';
 import api from '../../config/api';
 import { AuthContext } from '../../contexts/AuthContext';
 import Button from '../../components/Button/Button';
 import BackupTable from '../../components/BackupTable/BackupTable';
 import Sidebar from '../../components/Sidebar/Sidebar';
-import Styles from './SettingsView-styles';
 
 const SettingsView = () => {
   const [backupList, setBackupList] = useState([]);
@@ -49,24 +44,16 @@ const SettingsView = () => {
 
   return (
     <BaseLayout>
-      <Styles>
-        <h1>Settings</h1>
-        <hr />
-        <Row>
-          <Col xl={3} md={3}>
-            <Sidebar navigationList={['Data/backup']} active={active} setActive={setActive} />
-          </Col>
-          <Col lg>
-            <div className="content">
-              <div className="title">
-                <h3>Data/backup</h3>
-                <Button color="yellow" onClick={() => requestBackup()}>Request backup</Button>
-              </div>
-              <BackupTable backupList={backupList} />
-            </div>
-          </Col>
-        </Row>
-      </Styles>
+      <h1 className="text-4xl font-semibold">Settings</h1>
+      <hr className="my-3" />
+      {/* <Sidebar navigationList={['Data/backup']} active={active} setActive={setActive} /> */}
+      <div className="content">
+        <div className="md:flex md:justify-between md:items-center">
+          <h3 className="text-3xl font-semibold my-3">Data/backup</h3>
+          <Button color="yellow" onClick={() => requestBackup()}>Request backup</Button>
+        </div>
+        <BackupTable backupList={backupList} />
+      </div>
     </BaseLayout>
   );
 };
