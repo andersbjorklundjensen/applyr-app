1d07e2fa2dbe9896ba7d23bc1c1d1ca838c648b3
root <andersbjorklund@live.com>
2020-11-28 12:07:29 +0000

    Change backup table styles

--- frontend/src/components/BackupTable/BackupTable.jsx
+++ frontend/src/components/BackupTable/BackupTable.jsx
@@ -1,5 +1,7 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react'
+
 import React from 'react';
-import Styles from './BackupTable-styles';
 import DownloadLink from '../DownloadLink/DownloadLink';
 import api from '../../config/api';
 import * as moment from 'moment';
@@ -7,30 +9,28 @@ import * as moment from 'moment';
 const BackupTable = ({ backupList }) => {
 
   return (
-    <Styles>
-      <table className="table">
-        <thead>
-          <tr className="table-header">
-            <td>File name</td>
-            <td>Created</td>
+    <table className="table-auto w-full">
+      <thead>
+        <tr className="border-b border-gray-400">
+          <td>File name</td>
+          <td>Created</td>
+        </tr>
+      </thead>
+      <tbody css={css`td { padding: 10px; }`}>
+        {backupList && backupList.map((backup, index) => (
+          <tr key={index}>
+            <td>
+              <div key={index}>
+                <DownloadLink url={`${api.API_URL}/backup/${backup._id}`} filename={backup.filename} />
+              </div>
+            </td>
+            <td>
+              {moment(backup.created).format('DD.MM.YYYY hh:mm')}
+            </td>
           </tr>
-        </thead>
-        <tbody>
-          {backupList && backupList.map((backup, index) => (
-            <tr key={index}>
-              <td>
-                <div key={index}>
-                  <DownloadLink url={`${api.API_URL}/backup/${backup._id}`} filename={backup.filename} />
-                </div>
-              </td>
-              <td>
-                {moment(backup.created).format('DD.MM.YYYY hh:mm')}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </Styles>
+        ))}
+      </tbody>
+    </table>
   )
 }
 
