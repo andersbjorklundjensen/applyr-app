ae366b3ce9d45ec39a14d008ce785420afd57ee5
root <andersbjorklund@live.com>
2020-11-28 10:46:26 +0000

    Fix trying to find multiple backups based of unique id

--- backend/src/routes/backup/download-backup.js
+++ backend/src/routes/backup/download-backup.js
@@ -2,7 +2,9 @@
 module.exports = async (req, res) => {
   const { backupId } = req.params;
 
-  const backup = await req.app.locals.db.models.backups.find({ _id: backupId, ownerId: res.locals.userId });
+  const backup = await req.app.locals.db.models.backups
+    .findOne({ _id: backupId, ownerId: res.locals.userId })
+    .lean();
 
   if (!backup) {
     return res.status(400).send('backup not available');
