f4c086fab52220408bd08292bd78c202c149ede0
root <andersbjorklund@live.com>
2020-11-28 12:46:49 +0000

    Change logout route
    Veryify the jwt, get the userId and logout the user.
    This is being done here, because the authCheck middleware, rejects jwts that have expired.

--- backend/src/routes/user/logout.js
+++ backend/src/routes/user/logout.js
@@ -1,7 +1,16 @@
 
+const util = require('util');
+const jwt = require('jsonwebtoken');
+
+const verifyJwt = util.promisify(jwt.verify);
+const config = require('../../config');
+
 module.exports = async (req, res) => {
+  const authToken = await verifyJwt(req.get('authorization'), config.JWT_SECRET)
+    .catch((err) => err);
+
   await req.app.locals.db.models.users
-    .updateOne({ _id: res.locals.userId }, { lastLogoutTime: Date.now() });
+    .updateOne({ _id: authToken.userId }, { lastLogoutTime: Date.now() });
 
   res.json({
     logout: true,
