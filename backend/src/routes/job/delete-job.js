7c96842beb0ad4ea654bb7cae2094a1e220c0c86
root <andersbjorklund@live.com>
2020-11-30 00:20:23 +0000

    Refactor delete job api route
    Indented code.
    Sending error messages as json now.
    Delete files from file server when deleting associated job.

--- backend/src/routes/job/delete-job.js
+++ backend/src/routes/job/delete-job.js
@@ -1,33 +1,36 @@
 
 const mongoose = require('mongoose');
-const fs = require('fs');
+const fileDb = require('../../fileDb')();
 
 module.exports = async (req, res) => {
   const jobId = req.params.id;
 
   if (!mongoose.Types.ObjectId.isValid(jobId)) {
-    return res.status(400).send('invalid job id');
+    return res.status(400).json({ message: 'invalid job id' });
   }
 
   const job = await req.app.locals.db.models.jobs
-    .findOne({ _id: jobId, ownerId: res.locals.userId }).lean();
+    .findOne({ _id: jobId, ownerId: res.locals.userId })
+    .lean();
 
   if (!job) {
     return res.status(400).send('job not found');
   }
 
-  const files = await req.app.locals.db.models.files.find({ jobId }).lean();
+  const files = await req.app.locals.db.models.files
+    .find({ jobId })
+    .lean();
 
   await Promise.all(files.map(async (file) => {
-    fs.unlink(`uploads/${file.path}`, (err) => {
-      if (err) throw err;
-    });
+    fileDb.removeObject('files', file.storedFilename);
 
-    await req.app.locals.db.models.files.deleteOne({ _id: file._id });
+    await req.app.locals.db.models.files
+      .deleteOne({ _id: file._id });
   }));
 
   await req.app.locals.db.models.jobs
-    .deleteOne({ _id: jobId, ownerId: res.locals.userId }).lean();
+    .deleteOne({ _id: jobId, ownerId: res.locals.userId })
+    .lean();
 
   res.json({
     success: true,
