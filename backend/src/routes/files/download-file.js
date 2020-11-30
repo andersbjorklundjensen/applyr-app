0602298e89ec46aa9ceb5b63cd59dec6865a2ba0
root <andersbjorklund@live.com>
2020-11-30 01:54:25 +0000

    Fix indentations, lean methods and vertical spacing

--- backend/src/routes/files/download-file.js
+++ backend/src/routes/files/download-file.js
@@ -2,11 +2,15 @@
 module.exports = async (req, res) => {
   const { id: fileId } = req.params;
 
-  const file = await req.app.locals.db.models.files.findOne({ _id: fileId }).lean();
-  if (!file) return res.status(400).send('file does not exist');
+  const file = await req.app.locals.db.models.files
+    .findOne({ _id: fileId })
+    .lean();
+  if (!file) return res.status(400).json({ message: 'file does not exist' });
 
-  const job = await req.app.locals.db.models.jobs.findOne({ _id: file.jobId, ownerId: res.locals.userId }).lean();
-  if (!job) return res.status(400).send('file is inaccessible');
+  const job = await req.app.locals.db.models.jobs
+    .findOne({ _id: file.jobId, ownerId: res.locals.userId })
+    .lean();
+  if (!job) return res.status(400).json({ message: 'file is inaccessible' });
 
   res.download(`uploads/${file.path}`);
 };
--- backend/src/routes/files/get-all-files-for-job.js
+++ backend/src/routes/files/get-all-files-for-job.js
@@ -3,8 +3,8 @@ module.exports = async (req, res) => {
   const { jobId } = req.params;
 
   const job = await req.app.locals.db.models.jobs
-    .findOne({ ownerId: res.locals.userId, _id: jobId });
-
+    .findOne({ ownerId: res.locals.userId, _id: jobId })
+    .lean();
   if (!job) return res.status(400).json({ message: 'job doesnt exist' });
 
   const files = await req.app.locals.db.models.files.find({ jobId });
