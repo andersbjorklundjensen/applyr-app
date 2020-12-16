05d3494d92818e19f1b294963266f6858081119e
root <andersbjorklund@live.com>
2020-12-16 16:34:36 +0000

    Add dependencies for typescript

--- backend/src/bin/www.js
+++
@@ -1,4 +0,0 @@
-const config = require('../config');
-const app = require('..')();
-
-app.listen(config.PORT, () => console.log(`Server listening on port ${config.PORT}`));
--- backend/src/routes/user/index.js
+++
@@ -1,9 +0,0 @@
-
-const router = require('express').Router();
-
-router.post('/api/user/login', require('./login'));
-router.post('/api/user/register', require('./register'));
-router.post('/api/user/logout', require('./logout'));
-router.post('/api/user/username', require('./username'));
-
-module.exports = router;
