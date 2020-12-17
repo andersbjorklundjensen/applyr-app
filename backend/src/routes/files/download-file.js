d1824a2d6cf919437b5fc7371c22d61c9ad2adc5
root <andersbjorklund@live.com>
2020-12-17 23:16:58 +0000

    Add logic for downloading files from minio server

--- backend/src/routes/files/download-file.js
+++ backend/src/routes/files/download-file.js
@@ -12,7 +12,13 @@ module.exports = async (req, res) => {
   const job = await req.app.locals.db.models.jobs
     .findOne({ _id: file.jobId, ownerId: res.locals.userId })
     .lean();
+
   if (!job) return res.status(400).json({ message: 'file is inaccessible' });
 
-  res.download(`uploads/${file.path}`);
+  fileDb.getObject('files', file.storedFilename, (err, fileStream) => {
+    if (err) return console.log(err);
+
+    fileStream.on('data', (chunk) => res.write(chunk))
+    fileStream.on('end', () => res.end())
+  });
 };
