9a29d4c0bd7d002a9c05d86707a3a8fe36f7d1a0
anders <andersbjorklund@live.com>
2021-02-05 15:51:13 +0100

    Add a error field for login errors

--- frontend/src/views/LoginView/LoginView.tsx
+++ frontend/src/views/LoginView/LoginView.tsx
@@ -53,7 +53,11 @@ const LoginView = () => {
       <div className="flex justify-center">
         <div className="text-center">
           <h1 className="text-4xl font-semibold">Log in</h1>
-          {error !== null && <div>{error}</div>}
+          {error !== null && (
+            <div className="border-2 border-red-400 bg-red-100 py-2 mt-4">
+              {error}
+            </div>
+          )}
           <form
             css={css`
               * {
