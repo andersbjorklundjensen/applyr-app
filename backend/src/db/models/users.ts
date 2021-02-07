d19fe011513cec9b436c468206acddba1c332670
anders <andersbjorklund@live.com>
2021-02-07 19:07:45 +0100

    Change extension and import/export method

--- backend/src/db/models/users.ts
+++ backend/src/db/models/users.ts
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
-module.exports = new mongoose.Schema({
+export default new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
   created: { type: Number, required: true },
