4eeb6315a36ba3fc7cd0b50517a7905a5fe5e804
anders <andersbjorklund@live.com>
2021-02-08 16:36:06 +0100

    Fix router in files index

--- backend/src/routes/files/index.ts
+++ backend/src/routes/files/index.ts
@@ -1,10 +1,12 @@
-const router = require('express').Router();
+import { Router } from 'express';
 import { authCheck } from '../../middleware/authCheck';
 import { busboy } from '../../middleware/busboy';
 import getAllFilesForJob from './get-all-files-for-job';
 import downloadFile from './download-file';
 import uploadFile from './upload-file';
 import deleteFile from './delete-file';
+
+const router = Router();
 
 router.get('/api/files/:jobId', authCheck, getAllFilesForJob);
 router.get('/api/file/:id', authCheck, downloadFile);
