273c282447776e9b651a09396b8aebd0725d70a1
anders <andersbjorklund@live.com>
2021-02-08 16:35:10 +0100

    Fix router in backup index

--- backend/src/routes/backup/index.ts
+++ backend/src/routes/backup/index.ts
@@ -1,8 +1,10 @@
-const router = require('express').Router();
+import Router from 'express';
 import { authCheck } from '../../middleware/authCheck';
 import getBackupList from './get-backup-list';
 import downloadBackup from './download-backup';
 import requestBackup from './request-backup';
+
+const router = Router();
 
 router.get('/api/backup/list', authCheck, getBackupList); // get list of all backups
 router.get('/api/backup/:backupId', authCheck, downloadBackup); // downloading backup
