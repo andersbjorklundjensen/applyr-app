b4ebf0c5818e1ab9afc1be4691bfbc7857426ba6
anders <andersbjorklund@live.com>
2021-02-08 16:31:31 +0100

    Change get all files route to typescript

--- backend/src/routes/files/get-all-files-for-job.ts
+++ backend/src/routes/files/get-all-files-for-job.ts
@@ -1,9 +1,12 @@
-module.exports = async (req, res) => {
+import { Request, Response } from 'express';
+
+export default async (req: Request, res: Response) => {
   const { jobId } = req.params;
 
   const job = await req.app.locals.db.models.jobs
     .findOne({ ownerId: res.locals.userId, _id: jobId })
     .lean();
+    
   if (!job) return res.status(400).json({ message: 'job doesnt exist' });
 
   const files = await req.app.locals.db.models.files.find({ jobId });
