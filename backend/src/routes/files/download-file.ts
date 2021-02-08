244418fdb8bd2e39c8067e47980df7f6ac99ff94
anders <andersbjorklund@live.com>
2021-02-08 16:29:11 +0100

    Change download file route to typescript

--- backend/src/routes/files/download-file.ts
+++ backend/src/routes/files/download-file.ts
@@ -1,12 +1,14 @@
 import initFileDb from '../../fileDb';
+import { Request, Response } from 'express';
 const fileDb = initFileDb();
 
-module.exports = async (req, res) => {
+export default async (req: Request, res: Response) => {
   const { id: fileId } = req.params;
 
   const file = await req.app.locals.db.models.files
     .findOne({ _id: fileId })
     .lean();
+    
   if (!file) return res.status(400).json({ message: 'file does not exist' });
 
   const job = await req.app.locals.db.models.jobs
--- backend/src/routes/files/index.ts
+++ backend/src/routes/files/index.ts
@@ -2,12 +2,12 @@ const router = require('express').Router();
 import { authCheck } from '../../middleware/authCheck';
 import { busboy } from '../../middleware/busboy';
 import getAllFilesForJob from './get-all-files-for-job';
-
+import downloadFile from './download-file';
 
 import deleteFile from './delete-file';
 
 router.get('/api/files/:jobId', authCheck, getAllFilesForJob);
-router.get('/api/file/:id', authCheck, require('./download-file'));
+router.get('/api/file/:id', authCheck, downloadFile);
 router.post('/api/file/:jobId', authCheck, busboy, require('./upload-file'));
 router.delete('/api/file/:id', authCheck, deleteFile);
 
