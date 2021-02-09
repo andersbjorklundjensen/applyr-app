b310585aec2f084224dd20e46ff6d0eaef466a2c
anders <andersbjorklund@live.com>
2021-02-09 02:02:30 +0100

    Fix username route

--- backend/src/routes/user/username.ts
+++ backend/src/routes/user/username.ts
@@ -4,8 +4,8 @@ import isUsernameValid from './utils/validation/isUsernameValid';
 export default async (req: Request, res: Response) => {
   const { username } = req.body;
 
-  if (isUsernameValid(username))
-    return res.status(400).json({ message: 'Username is taken' });
+  if (!isUsernameValid(username))
+    return res.status(400).json({ message: 'invalid username' });
 
   const exists = !!(await req.app.locals.db.models.users.findOne({ username }));
 
