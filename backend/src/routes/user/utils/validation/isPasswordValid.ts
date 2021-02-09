26c3c64b531436149df47b9abc3821f801f2df64
anders <andersbjorklund@live.com>
2021-02-09 14:14:55 +0100

    Change register test to typescript

--- backend/src/routes/user/utils/validation/isPasswordValid.ts
+++ backend/src/routes/user/utils/validation/isPasswordValid.ts
@@ -1,4 +1,5 @@
 export default (password: string): boolean => {
+  if (!password) return false;
   if (password.length > 30) return false;
   if (password.length < 8) return false;
   return true;
--- backend/src/routes/user/utils/validation/isUsernameValid.ts
+++ backend/src/routes/user/utils/validation/isUsernameValid.ts
@@ -1,4 +1,5 @@
 export default (username: string) => {
+  if (!username) return false;
   if (username.length === 0) return false;
   if (username.length > 30) return false;
   else return true;
