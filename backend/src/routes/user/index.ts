1f0c86de7d34996cbf22486207a67ff15ac22418
anders <andersbjorklund@live.com>
2021-02-09 14:38:20 +0100

    Change logout route to typescript

--- backend/src/routes/user/index.ts
+++ backend/src/routes/user/index.ts
@@ -3,12 +3,13 @@ import login from './login';
 import register from './register';
 import logout from './logout';
 import username from './username';
+import { authCheck } from '../../middleware/authCheck';
 
 const router = Router();
 
 router.post('/api/user/login', login);
 router.post('/api/user/register', register);
-router.post('/api/user/logout', logout);
+router.post('/api/user/logout', authCheck, logout);
 router.post('/api/user/username', username);
 
 export default router;
