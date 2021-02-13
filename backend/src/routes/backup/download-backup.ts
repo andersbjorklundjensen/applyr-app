25974e8dbbab3cb41a43f481a75fe7146f33905b
anders <andersbjorklund@live.com>
2021-02-13 22:52:24 +0100

    Change api responses for backup api

--- backend/src/routes/backup/download-backup.ts
+++ backend/src/routes/backup/download-backup.ts
@@ -10,7 +10,7 @@ export default async (req: Request, res: Response) => {
     .lean();
 
   if (!backup) {
-    return res.status(400).send('backup not available');
+    return res.status(400).json({ message: 'backup not available' });
   }
 
   fileDb.getObject('backups', backup.filename, (err, fileStream) => {
